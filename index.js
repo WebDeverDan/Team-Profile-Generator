376ac378882bf798bb778d9dfd53b41b41a8a04d
Daniel Bradley <danielbvideo@gmail.com>
2021-08-11 17:39:22 -0400

    initial push - set up file structure

---
+++ index.js
@@ -0,0 +1,13 @@
+// Code needed: 
+// Inquirer NPM, Jest NPM
+
+// code is invoked via node index.js
+// Inquirer runs, asking for the following information:
+// Team Manager's Name, Employee ID, Email Address, Office Number
+// After the questions have been answered, there's a menu with the option to add engineer, add an intern, or finish building the team
+// if engineer is selected, namee, ID, email, GitHub Username are required
+// after entering information, taken back to the menu. 
+// if an intern is selected, name, ID, email, and school are needed
+// after entering information, taken back to the menu
+// upon completion, the team will be built and html will be generated
+ 
