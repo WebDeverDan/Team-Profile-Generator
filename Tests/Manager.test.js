f307d92335eae5248f4fc17d691002b2ff80dbf5
Daniel Bradley <danielbvideo@gmail.com>
2021-08-17 10:24:14 -0400

    updated and ran Managertests

---
+++ Tests/Manager.test.js
@@ -0,0 +1,35 @@
+const Employee = require('../lib/Employee')
+const Manager = require('../lib/Manager')
+
+test("getName", () => {
+    const name = "Daniel Bradley";
+    const output = new Employee(name);
+    expect(output.name).toEqual(name);
+
+});
+
+test("getId", () => {
+    const name = "Daniel Bradley";
+    const id = 34;
+    const output = new Employee(name,id);
+    expect(output.id).toEqual(id);
+
+});
+
+test("getEmail", () => {
+    const name = "Daniel Bradley";
+    const id = 34;
+    const email = "danielbvideo@gmail.com";
+    const output = new Employee(name, id, email);
+    expect(output.email).toEqual(email);
+
+});
+
+test("getGitHub", () => {
+    const name = "Daniel Bradley";
+    const id = 34;
+    const email = "danielbvideo@gmail.com";
+    const officeNumber = 24;
+    const output = new Manager(name, id, email, officeNumber);
+    expect(output.officeNumber).toEqual(officeNumber);
+});
